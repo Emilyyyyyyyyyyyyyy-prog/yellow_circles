a5889f9737a68273dee329bbf0f62f3d3bccab2e
Emilyyyyyyyyyyyyyy-prog <amaliahtsvetkova@yandex.ru>
2019-12-08 22:34:01 +0300

    конвертирован файл UI.ui в UI.py файл UI.py передан в main.py в виде класса Ui_Form

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from random import randint
-from PyQt5 import uic
-from PyQt5.QtGui import QBrush, QPainter, QColor, QPalette
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QBrush, QPainter, QColor, QPalette, QFont
+from PyQt5.QtCore import Qt, QRect, QMetaObject, QCoreApplication
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 
 
-class YellowCircle(QWidget):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('UI.ui', self)
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.pushButton = QPushButton(Form)
+        self.pushButton.setGeometry(QRect(10, 10, 381, 51))
+        font = QFont()
+        font.setPointSize(19)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Построить окружность"))
+
+
+class YellowCircle(QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
         self.setGeometry(300, 300, 400, 400)
         self.setWindowTitle('Окружность')
